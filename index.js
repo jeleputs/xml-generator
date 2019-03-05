5d31b1d8be7046f5bedb3a852ab768e919605226
Ricardo Aguilera-Zamarripa <r.aguilera.zamarripa@accenture.com>
2019-03-05 17:16:56 -0600

    wip Creating json builder

--- index.js
+++ index.js
@@ -13,3 +13,43 @@ const jsontoxml = () => {
 const cargarJson = () => {
   document.getElementById('json').value = JSON.stringify(jsonCertificado);
 };
+
+const addElement = () => {
+  var wrapper;
+
+  wrapper = convert.addElement({
+    name: 'doc',
+    content: 'id'
+  });
+
+  jsonChido = convert.addElement({
+    name: '$prueba',
+    content: 'bonita',
+    wrapper
+  });
+
+  alumno = convert.addElement({
+    name: 'Alumno',
+    content: {
+      $nombre: 'ricardo',
+      $apellido: 'aguilera',
+      $ap2: 'zamarripa',
+      $rfc: 'auzr88'
+    },
+    jsonChido
+  });
+
+  firmante = convert.addElement({
+    name: 'Firmante',
+    content: {
+      $lugar: 'saltillo',
+      $certificado: '123123123',
+      $firma: 'asfdasdfasf',
+      $fecha: '2019-01-01'
+    },
+    jsonChido
+  });
+
+  //jsonChido['oiiiink'] = jsonChido2;
+  document.getElementById('json').value = JSON.stringify(wrapper);
+};
--- js/jsontoxml.js
+++ js/jsontoxml.js
@@ -16,7 +16,19 @@ const xmltojsonFunc = input => {
   }
 };
 
+var json = {};
+
+const addElementFunc = params => {
+  var name = params.name;
+  var content = params.content;
+  var jsonNode = params.jsonNode || json;
+
+  jsonNode[name] = content;
+  return jsonNode;
+};
+
 convert = {
   jsontoxml: jsontoxmlFunc,
-  xmltojson: xmltojsonFunc
+  xmltojson: xmltojsonFunc,
+  addElement: addElementFunc
 };
